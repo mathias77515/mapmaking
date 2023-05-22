2b503da70989b5d292b13dbe2f888465cd119ccf
mregnier <mathias.p.regnier@gmail.com>
2023-05-22 16:08:33 +0200

    update source files

---
+++ src/frequency_map_making/FMM.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=both_001
+
+# we ask for n MPI tasks with N cores each on c nodes
+
+#SBATCH --partition=hpc
+#SBATCH --nodes=1                # c
+#SBATCH --ntasks-per-node=6      # n
+#SBATCH --cpus-per-task=5        # N
+#SBATCH --mem=20G
+#SBATCH --time=0-00:30:00
+#SBATCH --output=mulitple_jobs_%j.log
+
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+
+module load mpich
+
+mpirun -np $SLURM_NTASKS python FMM.py $1 $2 $3 $4 $5 $6
